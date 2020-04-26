0ae5261579fce90773faf92bae9b66bb65a0c3da
Murilo de Jesus Santos Silva <murilojssilva@outlook.com>
2020-04-26 19:01:59 -0300

    Add cors

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import routes from "./routes";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 class App {
   constructor() {
@@ -17,6 +18,7 @@ class App {
   }
   routes() {
     this.server.use(routes);
+    this.server.use(cors);
   }
 }
 
